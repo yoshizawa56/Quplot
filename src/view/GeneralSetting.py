af54654ce82271b8753d4b92807d3e356864498d
吉沢徹 <T.Yoshizawa@Yoshizawa.local>
2018-11-03 19:37:35 +0900

    comboBoxのデフォルト値の設定方法を変更

--- src/view/GeneralSetting.py
+++ src/view/GeneralSetting.py
@@ -8,7 +8,7 @@
 __author__ = "T.Yoshizawa <toru.yoshi.5.1@gmail.com>"
 __status__ = "production"
 __version__ = "0.1.0"
-__date__    = "02 November 2018"
+__date__    = "03 November 2018"
 
 from PyQt5.QtWidgets import (QWidget, QPushButton, QVBoxLayout,
         QLabel, QLineEdit, QHBoxLayout, QCheckBox, QComboBox)
