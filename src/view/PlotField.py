d19c62933b3d771537cc47bb2ea9f1b31296cedd
吉沢徹 <T.Yoshizawa@Yoshizawa.local>
2018-11-03 19:54:22 +0900

    config周りの実装

--- src/view/PlotField.py
+++ src/view/PlotField.py
@@ -29,24 +29,17 @@ class PlotField(QWidget):
         self.plot_button.clicked.connect(self.plot)
 
         #テスト用
-        d = {
-            'title' : 'test plot',
-            'title_font' : '8',
-            'legend_status' : 2,
-            'legend_position' : 2
-        }
+        d = Util.load_default_config()
         self.setting.set_default_config(d)
         
     def setup_ui(self):
         #左側
-        self.setting = GeneralSetting()
         self.data_tab = DataTab()
         self.x_axis = AxisSetting('X')
         self.y_axis = AxisSetting('Y')
         self.plot_button = QPushButton('Plot!!')
         #TODO Widgetの追加
         left_widgets = [
-            #self.setting,
             self.data_tab,
             Util.Hbox([self.x_axis, self.y_axis]),
             self.plot_button
@@ -54,6 +47,7 @@ class PlotField(QWidget):
         
         #右側
         #TODO Widgetの追加
+        self.setting = GeneralSetting()
         self.canvas = Canvas()
         right_widgets = [
             self.setting,
@@ -69,6 +63,25 @@ class PlotField(QWidget):
                 ]
             )
         )
+
+        #設定を扱うwidgetのリスト
+        self.widget_list = [
+            self.data_tab,
+            self.x_axis,
+            self.y_axis,
+            self.setting
+        ]
+
+    def config_dict(self):
+        config = {}
+        for widget in self.widget_list:
+            config.update(widget)
+
+        return config
+
+    def set_default_config(self, default_config_dict):
+        for widget in self.widget_list:
+            widget.set_default_config(default_config_dict)
 
     def plot(self):
         Plot.execute(self.canvas, [])
